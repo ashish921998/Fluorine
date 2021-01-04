91128ad39497fac10e5b3481b16fba25c5da1a8b
Rohit kumar <rohitagile@gmail.com>
2021-01-04 12:23:56 +0530

    removed unwanted code

--- SignUp.js
+++ SignUp.js
@@ -8,22 +8,3 @@ function store() {
     localStorage.setItem('pw', pw.value);
     localStorage.setItem('email',email.value )
 }
-
-// // check if stored data from register-form is equal to entered data in the   login-form
-// function check() {
-
-//     // stored data from the register-form
-//     var storedName = localStorage.getItem('name1');
-//     var storedPw = localStorage.getItem('pw');
-
-//     // entered data from the login-form
-//     var userName = document.getElementById('userName');
-//     var userPw = document.getElementById('userPw');
-
-//     // check if stored data from register-form is equal to data from login form
-//     if(userName.value == storedName && userPw.value == storedPw) {
-//         alert('You are loged in.');
-//     }else {
-//         alert('ERROR.');
-//     }
-// }
